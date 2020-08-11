a0a6301053cfd6a7b031ebee7610397a2aa42e7b
Marcos Azevedo <marcos.azevedo@izap.com.br>
2020-08-11 11:48:49 -0300

    chore: config react

---
+++ src/main/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App: React.FC = () => {
+  return <p>Oi</p>
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('main')
+)
