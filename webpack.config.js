7a226210152bdb8ef2439f303aee5dc70d597950
Marcos Azevedo <marcos.azevedo@izap.com.br>
2020-08-11 23:12:23 -0300

    fix: config svg file

--- webpack.config.js
+++ webpack.config.js
@@ -24,14 +24,7 @@ module.exports = {
       },
       {
         test: /\.svg$/,
-        use: [
-          {
-            loader: 'svg-url-loader',
-            options: {
-              limit: 10000,
-            }
-          }
-        ]
+        use: ['@svgr/webpack', 'url-loader']
       },
       {
         test: /\.scss$/,
